77fd772275eeef83dd6cb932cf93f825f197b877
bennisan86 <info@bennisan.be>
2020-01-16 12:41:12 +0100

    So far so good

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps'
+import * as parksData from './data/skateparks.json';
+
+function Map(){
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      // defaultCenter={{lat: 51.219448, lng: 4.402464}}>
+      defaultCenter={{lat: 45.421532, lng: -75.697189}}>
+      {parksData.features.map((park) => (
+          <Marker 
+            key={park.properties.PARK_ID}
+            position={{
+              lat: park.geometry.coordinates[1],
+              lng: park.geometry.coordinates[0]}} />
+        )
+        )}
+      </GoogleMap>
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+        loadingElement={<div style={{ height: "100%" }}></div>}
+        containerElement={<div style={{ height: "100%" }}></div>}
+        mapElement={<div style={{ height: "100%" }}></div>}
+      />
     </div>
   );
 }
